7c2d8e3b313c5793d742f2ef4f0724874e62f7dc
jkaloger <jkaloger@users.noreply.github.com>
2017-04-26 18:02:24 +1000

    added json parser

--- src/languageIdentifier.py
+++ src/languageIdentifier.py
@@ -2,8 +2,19 @@
 # Jack Kaloger 2017
 # COMP30027 Project 2
 
+import json
+
+
+def read_json(filename):
+    a = []
+    for line in open(filename):
+        a.append(json.loads(line))
+    return a
+
+
 def main():
-    return 0
+    data = read_json('in/dev.json')
+    print(data)
 
 
 if __name__ == "__main__":
